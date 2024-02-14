faba09e8155c82b8297d9f657e903efca8994d1a
Praveenkumar21v <151128238+Praveenkumar21v@users.noreply.github.com>
2024-02-14 21:29:40 +0530

    Update Nopage.js

--- Pages/Nopage.js
+++ Pages/Nopage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nopage = () => {
   return (
-    <h1 style={{textAlign:"center" , color:"red"}}>404 Page Error</h1>
+    <h1 style={{textAlign:"center" , color:"red", marginTop:"10%"}}>404 Page Error</h1>
   )
 }
 
